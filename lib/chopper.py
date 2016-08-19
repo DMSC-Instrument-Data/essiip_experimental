ec73447a6c1d1412656eaf4e4df4fbc702aae190
Michael Wedel <michael.wedel@esss.se>
2016-08-19 11:21:02 +0200

    Mini chopper setup and lib

---
+++ lib/chopper.py
@@ -0,0 +1,87 @@
+from nicos.devices.epics import EpicsReadable, EpicsMoveable, pvname, EpicsDevice
+from nicos.core import status, Param, Override, Attach, usermethod, HasLimits, Moveable, SIMULATION, Value, tupleof
+
+
+class EpicsFloatMoveable(EpicsMoveable):
+    """
+    Handles EPICS devices which can set and read a float value, but without limits.
+    """
+    valuetype = float
+
+
+class EpicsStringMoveable(EpicsMoveable):
+    """
+    Handles EPICS devices which can set and read a string value.
+    """
+    valuetype = str
+
+
+class EssChopper(Moveable):
+    attached_devices = {
+        'speed': Attach('Speed of the chopper disc.', EpicsMoveable),
+        'phase': Attach('Phase of the chopper disc', EpicsMoveable),
+        'parkposition': Attach('Position in parked state', EpicsMoveable),
+        'state': Attach('Current state of the chopper', EpicsReadable, optional=True),
+        'command': Attach('Command PV of the chopper', EpicsMoveable)
+    }
+
+    state_map = {
+        'init': (status.ERROR, 'Interlocks not fulfilled'),
+        'stopped': (status.OK, 'Waiting for commands'),
+        'parked': (status.OK, 'Parked'),
+        'parking': (status.BUSY, 'Moving to park position'),
+        'accelerating': (status.BUSY, 'Adjusting speed to target'),
+        'phase_locking': (status.BUSY, 'Acquiring phase lock'),
+        'phase_locked': (status.OK, 'Speed and phase locked'),
+        'stopping': (status.BUSY, 'Decelerating disc'),
+        'idle': (status.OK, 'Disc rotating freely, waiting for command.'),
+        'bearings': (status.BUSY, 'Initialising bearings'),
+    }
+
+    parameter_overrides = {
+        'fmtstr': Override(default='%.2f %.2f'),
+        'unit': Override(mandatory=False),
+    }
+
+    hardware_access = False
+    valuetype = tupleof(float, float)
+
+    def doRead(self, maxage=0):
+        return [self._attached_speed.read(maxage), self._attached_phase.read(maxage)]
+
+    def doStart(self, pos):
+        if hasattr(self, '_attached_state') and self._attached_state.read() == 'init':
+            self.initialize()
+
+        self._attached_speed.move(pos[0])
+        self._attached_phase.move(pos[1])
+        self._attached_command.move('start')
+
+    def doStop(self):
+        self._attached_command.move('stop')
+
+    # def doReadAbslimits(self):
+    #    return [(0.0, 40.0), (0.0, 360.0)]
+
+    def doStatus(self, maxage=0):
+        if hasattr(self, '_attached_state'):
+            return self.state_map[self._attached_state.read()]
+
+        return status.WARN, 'State PV is missing, no reliable state information.'
+
+    @usermethod
+    def initialize(self):
+        self._attached_command.move('init')
+
+    @usermethod
+    def deinitialize(self):
+        self._attached_command.move('deinit')
+
+    @usermethod
+    def parkAt(self, position):
+        self._attached_parkposition.move(position)
+        self._attached_command.move('park')
+
+    @usermethod
+    def unlock(self):
+        self._attached_command.move('unlock')
---
+++ setups/mini_chopper.py
@@ -0,0 +1,66 @@
+description = 'Mini chopper in the ESSIIP lab.'
+
+
+def get_chopper_configuration(chopper_pv_base, chopper_number):
+    chopper_base_pv = chopper_pv_base.format(chopper_number)
+
+    return {
+        '{}_speed'.format(chopper_base_pv):
+            device('essiip_experimental.chopper.EpicsFloatMoveable',
+                   pollinterval=0.5,
+                   abslimits=(0, 14),
+                   description='Speed control of chopper {}'.format(chopper_number),
+                   readpv='{}:Spd-RB'.format(chopper_base_pv),
+                   writepv='{}:Spd'.format(chopper_base_pv),
+                   lowlevel=True),
+
+        '{}_phase'.format(chopper_base_pv):
+            device('essiip_experimental.chopper.EpicsFloatMoveable',
+                   pollinterval=0.5,
+                   abslimits=(0, 360),
+                   description='Phase control of chopper {}'.format(chopper_number),
+                   readpv='{}:Phs-RB'.format(chopper_base_pv),
+                   writepv='{}:Phs'.format(chopper_base_pv),
+                   lowlevel=True
+                   ),
+
+        '{}_park'.format(chopper_base_pv):
+            device('essiip_experimental.chopper.EpicsFloatMoveable',
+                   pollinterval=0.5,
+                   abslimits=(0, 360),
+                   description='Park control of chopper {}'.format(chopper_number),
+                   readpv='{}:ParkAng-RB'.format(chopper_base_pv),
+                   writepv='{}:ParkAng'.format(chopper_base_pv),
+                   lowlevel=True
+                   ),
+
+        # '{}_state'.format(chopper_base_pv):
+        #     device('devices.epics.EpicsReadable',
+        #            pollinterval=0.5,
+        #            description='State information of chopper {}'.format(chopper_number),
+        #            readpv='{}:State'.format(chopper_base_pv),
+        #            lowlevel=True
+        #            ),
+
+        '{}_command'.format(chopper_base_pv):
+            device('essiip_experimental.chopper.EpicsStringMoveable',
+                   description='Command interface of chopper {}'.format(
+                       chopper_number),
+                   readpv='{}:Cmd'.format(chopper_base_pv),
+                   writepv='{}:Cmd'.format(chopper_base_pv),
+                   lowlevel=True
+                   ),
+
+        chopper_base_pv:
+            device('essiip_experimental.chopper.EssChopper',
+                   description='Chopper {}'.format(chopper_number),
+                   speed=['{}_speed'.format(chopper_base_pv)],
+                   phase=['{}_phase'.format(chopper_base_pv)],
+                   parkposition=['{}_park'.format(chopper_base_pv)],
+                   # state=['{}_state'.format(chopper_base_pv)],
+                   command=['{}_command'.format(chopper_base_pv)]
+                   )
+    }
+
+
+devices = get_chopper_configuration('LabS-ESSIIP:Chop-CHIC-{:02d}', 1)
